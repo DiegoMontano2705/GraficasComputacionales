1bc2e7ebacd4eda19cf7325ab1c015fad9542f43
Diego Montaño <31355652+DiegoMontano2705@users.noreply.github.com>
2021-02-19 13:33:39 -0600

    First class exercise

---
+++ 1erParcial/THREE.JS/js/main.js
@@ -0,0 +1,47 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js'; // in case you install THREE.Js change Path
+let renderer,scene,camera, mesh
+
+function init(){
+  // Renderer
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor("red");  // renderer.setClearColor(new THREE.Color(0,0,0));  to use RGB
+  document.body.appendChild(renderer.domElement);
+
+  // Scene
+  scene = new THREE.Scene();
+  
+  // Virtual Camera
+  let fov = 60;
+  let aspect = window.innerWidth / window.innerHeight;
+  let near = 0.1;
+  let far = 10000;
+  camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
+  camera.position.set(0,0,3);
+
+  // Models
+  let geometry = new THREE.BoxGeometry();
+  let material = new THREE.MeshBasicMaterial({color: "blue" ,wireframe: true});
+  mesh = new THREE.Mesh(geometry,material);
+
+  // Scene Hierarchy/Graph
+  scene.add(mesh);
+
+  // Render Loop
+  renderLoop();
+
+}
+
+function renderLoop(){
+  // Draw Scene
+  renderer.render(scene,camera);
+  updateScene();
+  requestAnimationFrame(renderLoop);
+}
+
+function updateScene(){
+  mesh.rotation.y = mesh.rotation.y + 1 + Math.PI / 180;
+}
+
+// Event Listeners/Handlers
+document.addEventListener("DOMContentLoaded",init);
